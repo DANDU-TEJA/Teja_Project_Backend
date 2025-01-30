f768e61ca6986b7b861661980aaa24d7f3cb9c9e
DANDU-TEJA <tejadandu012@gmail.com>
2025-01-30 14:27:26 +0530

    Updated 3rd time by removing path package

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const farmerRoutes =require('./routes/farmerRoutes')
 const riceRoutes=require('./routes/riceRoutes')
 const productRoutes=require('./routes/productRoutes');
 
-const cors=require('cors');
+//const cors=require('cors');
 
-const path=require('path');
+//const path=require('path');
 
 //now convert the text into json format for that we need body parser
 const bodyParser = require('body-parser');
@@ -16,13 +16,25 @@ const bodyParser = require('body-parser');
 const dotEnv=require("dotenv");
 const mongoose=require("mongoose");
 
- app.use(cors());
-dotEnv.config()
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("Teja Mongo DB Connected Successfully"))
-.catch((error)=>console.log(error))
+// app.use(cors());
 
-const PORT=process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
+dotEnv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("Teja Mongo DB Connected Successfully"))
+    .catch((error)=>console.log(error))
+
+    app.use(bodyParser.json());
+    //Now To Use The HTTP methods We Need The Middle Wares
+    app.use('/farmer',farmerRoutes);
+    
+    app.use('/rice',riceRoutes);
+    
+    app.use('/product',productRoutes); 
+   // app.use('/uploads',express.static('uploads'));
+    
+
 app.listen(PORT,()=>{
     console.log(`Teja Server Started At ${PORT}`);
 });
@@ -31,11 +43,3 @@ app.use('/',(req,res)=>{
     res.send("<h1>Teja Welcome To MERN Project");
 });
 
-app.use(bodyParser.json());
-//Now To Use The HTTP methods We Need The Middle Wares
-app.use('/farmer',farmerRoutes);
-
-app.use('/rice',riceRoutes);
-
-app.use('/product',productRoutes); 
-app.use('/uploads',express.static('uploads'));
